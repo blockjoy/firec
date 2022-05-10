74bce54b9d39e5f60e922ccfbde2b3a65ab81012
Zeeshan Ali <zeeshanak@gnome.org>
2022-05-10 19:00:29 +0200

    Add more Error variants

--- src/error.rs
+++ src/error.rs
@@ -6,4 +6,36 @@ pub enum Error {
     /// Failed to generate UUID.
     #[error("Failed to generate UUID")]
     Uuid(#[from] uuid::Error),
+
+    /// IO error.
+    #[error("IO error")]
+    Io(#[from] std::io::Error),
+
+    /// Hyper error.
+    #[error("Hyper error")]
+    Hyper(#[from] hyper::Error),
+
+    /// HTTP error.
+    #[error("HTTP error")]
+    Http(#[from] hyper::http::Error),
+
+    /// Invalid Jailer executable path specified.
+    #[error("Invalid Jailer executable path specified")]
+    InvalidJailerExecPath,
+
+    /// Invalid initrd path specified.
+    #[error("Invalid initrd path specified")]
+    InvalidInitrdPath,
+
+    /// Invalid socket path specified.
+    #[error("Invalid socket path specified")]
+    InvalidSocketPath,
+
+    /// Invalid drive path specified.
+    #[error("Invalid drive path specified")]
+    InvalidDrivePath,
+
+    /// Invalid chroot base path specified.
+    #[error("Invalid chroot base path specified")]
+    InvalidChrootBasePath,
 }
