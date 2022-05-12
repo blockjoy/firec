a565ed3d09d0739f2bb5f7e1193fb60555f0933c
Zeeshan Ali <zeeshanak@gnome.org>
2022-05-12 15:40:12 +0200

    Fix machine config fields

--- src/config/machine.rs
+++ src/config/machine.rs
@@ -1,24 +1,33 @@
 use std::borrow::Cow;
 
+use derivative::Derivative;
 use serde::{Deserialize, Serialize};
 
 /// Machine configuration.
 // TODO: Provide a builder for `Machine`.
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Derivative, Debug, Serialize, Deserialize, Default)]
 pub struct Machine<'m> {
-    /// Flag for enabling/disabling Hyperthreading
-    ht_ennabled: bool,
+    /// Flag for enabling/disabling simultaneous multithreading. Can be enabled only on x86.
+    pub smt: bool,
+
+    /// Enable dirty page tracking. If this is enabled, then incremental guest memory snapshots
+    /// can be created. These belong to diff snapshots, which contain, besides the microVM state,
+    /// only the memory dirtied since a previous snapshot. Full snapshots each contain a full copy
+    /// of the guest memory.
+    pub track_dirty_pages: bool,
 
     /// Memory size of VM
-    mem_size_mib: i64,
+    pub mem_size_mib: i64,
 
     /// Number of vCPUs (either 1 or an even number)
     ///
     /// Maximum: 32
     /// Minimum: 1
-    vcpu_count: i64,
+    #[derivative(Default(value = "1"))]
+    pub vcpu_count: usize,
 
     /// cpu template
     // TODO: Should create a type to validate it like the Go API.
-    cpu_template: Option<Cow<'m, str>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cpu_template: Option<Cow<'m, str>>,
 }
